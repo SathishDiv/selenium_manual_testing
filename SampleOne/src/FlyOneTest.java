39c352b1c3f713931d1aa902a1ee1e895074dde2
sathishsuriya <sathishsuriya@gmail.com>
2021-11-26 17:02:55 +0530

    correct version

---
+++ SampleOne/src/FlyOneTest.java
@@ -0,0 +1,16 @@
+
+public class FlyOneTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("nissan");
+		
+		System.out.println("tata");
+		
+		System.out.println("tcs");
+		
+		System.out.println("cts");
+	}
+
+}
